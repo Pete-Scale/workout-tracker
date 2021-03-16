50214c8981858131c0c44e92b2782d723400b27a
Pete-Scale <petescale@gmail.com>
2021-03-15 21:12:29 -0500

    added routes folder, api.js, post route

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.post("/api/workouts", ({ body }, res) => {
+    console.log(body);
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// router.put("/api/workouts/:id",)
+
+// router.get("/api/workouts",)
+
