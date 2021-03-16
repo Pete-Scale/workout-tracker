4a677ed28f78cdbc8a69f6f8f031699ee328de39
Pete-Scale <petescale@gmail.com>
2021-03-16 17:02:28 -0500

    range route

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.post("/api/workouts", ({ body }, res) => {
-    console.log(body);
-    db.Workout.create(body)
+    // console.log(body);
+    db.Workout.create({body})
         .then(data => {
             res.json(data);
         })
@@ -27,8 +27,21 @@ router.put("/api/workouts/:id", ({ params, body }, res) => {
 });
 
 router.get("/api/workouts", ({ body }, res) => {
-    console.log(body);
-    db.Workout.find({})
+    // console.log(body);
+    db.Workout.find({body})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", ({ body }, res) => {
+    // console.log(body);
+    db.Workout.find({body})
+        .limit(7)
         .then(data => {
             res.json(data);
         })
