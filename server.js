b01cdfc99eac60b5f5725fb06e5fe545bb50e46e
Pete-Scale <petescale@gmail.com>
2021-03-16 21:14:56 -0500

    updated database connection

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
     {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
         useFindAndModify: false
     }
 );
